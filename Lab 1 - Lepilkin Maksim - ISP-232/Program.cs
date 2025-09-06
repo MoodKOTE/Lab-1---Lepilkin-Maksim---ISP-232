58de0a1ed00ae1b056c4acc7c83bc0c6bebb82bf
Лепилкин Максим <maksimlepilkin7@gmail.com>
2025-09-07 01:20:25 +0300

    Добавьте файлы проекта.

---
+++ Lab 1 - Lepilkin Maksim - ISP-232/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab_1___Lepilkin_Maksim___ISP_232
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // задание 1
+            string game = "Dying Light";
+            Console.WriteLine(game);
+            int num = 7;
+            Console.WriteLine(num);
+            double pi = 3.14;
+            Console.WriteLine(pi);
+            string alf = "f";
+            Console.WriteLine(alf);
+
+            // задание 2
+            Console.WriteLine("I need more power!");
+            Console.WriteLine("I need more power!");
+            Console.WriteLine("I need more power!");
+            Console.WriteLine("I need more power!");
+             
+        }
+    }
+}
